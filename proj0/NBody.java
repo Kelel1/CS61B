f6466b2add0fc9339fabad8da8c482ba6cf940a7
Kern Elder <kelel1@yahoo.com>
2018-06-28 02:30:26 -0400

    Add readPlanets method

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -9,5 +9,29 @@ public class NBody {
         return radiusUniverse; 
    }
 
+   /** Return an array of Planets corresponding to the planets in the file */
+   public static Planet[] readPlanets(String fileName) {
+        In in = new In(fileName);
+        int totlPlanets = in.readInt();
+        Planet[] bodies = new Planet[totlPlanets];
+        double radius = in.readDouble(); 
+        
+        
+       for(int j = 0; j < bodies.length; j++ ) {      
+       double xxPos = in.readDouble();
+       double yyPos = in.readDouble();
+       double xxVel = in.readDouble();
+       double yyVel = in.readDouble();
+       double mass = in.readDouble();
+       String planetName = in.readString();
+       
+       Planet body = new Planet(xxPos, yyPos, xxVel,
+                               yyVel, mass, planetName);  
+
+       bodies[j] = body;
+  
+    } 
+       return bodies;
+   }
     
 }
