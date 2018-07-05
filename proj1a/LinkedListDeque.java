70000b657a3b2ca074992ee32f6886ed940564ce
Kern Elder <kelel1@yahoo.com>
2018-07-05 02:15:25 -0400

    Finish addFirst method

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,9 +1,46 @@
-public class LinkedListDeque {
+public class LinkedListDeque<T> {
+
+    private class DequeNode {
+        public DequeNode previous;
+        public T item;
+        public DequeNode next;
+
+
+        public DequeNode(DequeNode p, T i, DequeNode n) {
+            previous = p;
+             item = i;
+            next = n;
+
+        }
+    }
+
+    /** The first item (if it exists) is at sentinel.next or
+     *  sentinel.previous
+     */
+    private DequeNode sentinel;
+    private int size;
+
+    public LinkedListDeque() {
+        sentinel = new DequeNode(null, null, null);
+        size = 0;
+    }
+
+    public LinkedListDeque(T var) {
+        sentinel = new DequeNode(null, null, null);
+        sentinel.next = new DequeNode(sentinel, var, null);
+        size = 1;
+    }
+
 
 
     /** Adds an item of type T to the front of the deque. */
     public void addFirst(T item) {
+        if(sentinel.next == null) {
+            size += 1;
+            sentinel.next = new DequeNode(sentinel, item, null);
+        }
 
+        sentinel = sentinel.next;
     }
 
     /** Adds an item of type T to the back of the deque. */
@@ -13,11 +50,13 @@ public class LinkedListDeque {
 
     /** Returns true if deque is empty, false otherwise. */
     public boolean isEmpty() {
+        return true;
 
     }
 
     /** Returns the number of items in the deque. */
     public int size() {
+        return size;
 
     }
 
@@ -30,14 +69,14 @@ public class LinkedListDeque {
      * If no such item exists, returns null.
      */
     public T removeFirst() {
-
+        return null;
     }
 
     /** Removes and returns the item at the back of the deque.
      *  If no such item exists, returns null.
      */
     public T removeLast() {
-
+        return null;
     }
 
     /** Gets the item at the given indexm where 0 is the front,
@@ -45,6 +84,19 @@ public class LinkedListDeque {
      *  returns null.  Must not alter deque!
      */
     public T get(int index) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        LinkedListDeque<Integer> L = new LinkedListDeque<>();
+        L.addFirst(5);
+        L.addFirst(10);
+        L.addFirst(15);
+        System.out.println("size: " + L.size());
+
+
+
 
     }
 }
