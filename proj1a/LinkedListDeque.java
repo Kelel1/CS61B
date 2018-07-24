700c039d53aa30a8f788f0ded0357e4bb8195c20
Kern Elder <kelel1@yahoo.com>
2018-07-24 04:34:35 -0400

    Complete addFirst/addLast methods

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -18,16 +18,19 @@ public class LinkedListDeque<T> {
      *  sentinel.previous
      */
     private DequeNode sentinel;
+    private DequeNode pointer;
     private int size;
 
     public LinkedListDeque() {
-        sentinel = new DequeNode(null, null, null);
+        sentinel = new DequeNode(sentinel, null, sentinel);
+        pointer  = sentinel;
         size = 0;
     }
 
     public LinkedListDeque(T var) {
-        sentinel = new DequeNode(null, null, null);
-        sentinel.next = new DequeNode(sentinel, var, null);
+        sentinel = new DequeNode(sentinel, null, sentinel);
+        sentinel.next = new DequeNode(sentinel, var, sentinel);
+        pointer = sentinel.next;
         size = 1;
     }
 
@@ -35,34 +38,74 @@ public class LinkedListDeque<T> {
 
     /** Adds an item of type T to the front of the deque. */
     public void addFirst(T item) {
-        if(sentinel.next == null) {
-            size += 1;
-            sentinel.next = new DequeNode(sentinel, item, null);
-        }
 
-        sentinel = sentinel.next;
-    }
+        DequeNode newNode;
+        
+
+        if(sentinel.next == null) {
+            newNode = new DequeNode(sentinel, item, sentinel);            
+            sentinel.next = newNode;
+            sentinel.previous = newNode;
+            newNode.previous = sentinel;
+            newNode.next = sentinel;
+            pointer = newNode;           
+            size++;            
+                   
+        } else {         
+         
+            newNode = new DequeNode(sentinel, item, pointer);
+         
+            pointer.previous = newNode;            
+            sentinel.next = newNode;
+            pointer = newNode;            
+            size++;
+                     
+        }
+    } //complete
 
     /** Adds an item of type T to the back of the deque. */
     public void addLast(T item) {
-
+        DequeNode newNode;
+       
+        if(sentinel.previous == null) {            
+            newNode = new DequeNode(sentinel, item, sentinel);
+            sentinel.next = newNode;
+            sentinel.previous = newNode;
+            newNode.previous = sentinel;
+            newNode.next = sentinel;
+            pointer = newNode;           
+            size++; 
+        } else {            
+           
+           newNode = new DequeNode(pointer, item, sentinel);
+           pointer.next = newNode;
+           sentinel.previous = newNode;
+           pointer = newNode;
+           size++;           
+        
+        }        
     }
 
     /** Returns true if deque is empty, false otherwise. */
     public boolean isEmpty() {
-        return true;
-
-    }
+        if(sentinel.next == null) {
+            return true;
+        }
+        return false;
+    } // complete
 
     /** Returns the number of items in the deque. */
     public int size() {
         return size;
-
-    }
+    } //complete
 
     /** Prints the items in the deque from first to last, separated by a space. */
     public void printDeque() {
-
+        while(sentinel.previous != null) {
+            System.out.print(sentinel.item + " ");
+            sentinel = sentinel.previous;
+        }
+        System.out.println("");
     }
 
     /** Removes and returns the item at the front of the deque.
@@ -79,21 +122,46 @@ public class LinkedListDeque<T> {
         return null;
     }
 
-    /** Gets the item at the given indexm where 0 is the front,
+    /** Gets the item at the given index where 0 is the front,
      *  1 is the next item, and so forth.  If no such item exists,
      *  returns null.  Must not alter deque!
      */
     public T get(int index) {
+        if(index < size &&  index > -1) {
+
+            if(index == 0) {
+                return sentinel.item;
+            } else if(index > 0) {
+                for(int i = 0; i < index - 1; i++) {
+                    sentinel = sentinel.previous;
+                }
+                return sentinel.previous.item;
+            }
+
+        }
         return null;
     }
 
     public static void main(String[] args) {
 
         LinkedListDeque<Integer> L = new LinkedListDeque<>();
-        L.addFirst(5);
-        L.addFirst(10);
-        L.addFirst(15);
-        System.out.println("size: " + L.size());
+//       L.addLast(5);
+        L.addLast(10);
+        L.addLast(15);
+////        L.addLast(25);
+//        L.addFirst(30);
+         L.addFirst(35);
+         L.addFirst(22);
+         System.out.println(L.isEmpty());
+        // L.addFirst(86);
+    //     L.addFirst(84);
+    //     L.addFirst(53);
+    //    L.addFirst(66);
+       // System.out.println("size: " + L.size());
+      //  System.out.println(L.get(0));
+//        System.out.println(L.get(3));
+      // System.out.println(L.get(2));
+     //   L.printDeque();
 
 
 
