dbb50756accd4cf490ccfb5f03f9dcd06798b462
Kern Elder <kelel1@yahoo.com>
2018-07-25 11:40:26 -0400

    Complete initial buildout of methods

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,48 +1,86 @@
-public class ArrayDeque {
-    /** Adds an item of type T to the front of the deque. */
-    public void addFirst(T item) {
+public class ArrayDeque<Item> {
 
+    private Item[] items;
+    private int size;
+
+    public ArrayDeque() {
+        items = (Item[]) new Object[8];
+        size = 0;
     }
 
-    /** Adds an item of type T to the back of the deque. */
-    public void addLast(T item) {
-
-    }
-
-    /** Returns true if deque is empty, false otherwise. */
-    public boolean isEmpty() {
-
-    }
-
-    /** Returns the number of items in the deque. */
-    public int size() {
-
-    }
-
-    /** Prints the items in the deque from first to last, separated by a space. */
-    public void printDeque() {
-
-    }
-
-    /** Removes and returns the item at the front of the deque.
-     * If no such item exists, returns null.
+    /** Creates a new bigger array(2X), once original array
+     * has reached max capacity.
      */
-    public T removeFirst() {
-
+    private void resize(int capacity) {
+        Item[] a = (Item[]) new Object[capacity];
+        System.arraycopy(items, 0, a, 0, size);
     }
 
-    /** Removes and returns the item at the back of the deque.
-     *  If no such item exists, returns null.
-     */
-    public T removeLast() {
 
-    }
+   /** Adds an item of type T to the front of the deque. 
+    * If your pointer is a t postion 0, and you addfirst 
+    * the front pointer should loop back around to the end 
+    * of the array.
+    */
+   public void addFirst(Item item) {
 
-    /** Gets the item at the given indexm where 0 is the front,
-     *  1 is the next item, and so forth.  If no such item exists,
-     *  returns null.  Must not alter deque!
-     */
-    public T get(int index) {
+   }
 
-    }
+   /** Adds an item of type T to the back of the deque. */
+   public void addLast(Item item) {
+       if (size == items.length) {
+         resize(size*2);
+       }
+       items[size] = item;
+       size++;
+
+   }
+
+   /** Returns true if deque is empty, false otherwise. */
+   public boolean isEmpty() {       
+       return size == 0;
+   }
+
+   /** Returns the number of items in the deque. */
+   public int size() {       
+        return size;
+   }
+
+   /** Prints the items in the deque from first to last, separated by a space. */
+   public void printDeque() {
+     for(int i = 0; i < items.length; i++) {
+       System.out.print(items[i] + " ");
+     }
+
+   }
+
+   /** Removes and returns the item at the front of the deque.
+    * If no such item exists, returns null.
+    */
+   public Item removeFirst() {
+    return null;
+   }
+
+   /** Removes and returns the item at the back of the deque.
+    *  If no such item exists, returns null.
+    */
+   public Item removeLast() {
+      return null;
+   }
+
+   /** Gets the item at the given index where 0 is the front,
+    *  1 is the next item, and so forth.  If no such item exists,
+    *  returns null.  Must not alter deque!
+    */
+   public Item get(int index) {
+     return items[index];
+   }
+   
+   public static void main(String[] args) {
+     
+     ArrayDeque<String> numbs = new ArrayDeque<String>();
+     numbs.addLast("Hello Kern!");
+
+     System.out.println(numbs.get(0));
+   }
 }
