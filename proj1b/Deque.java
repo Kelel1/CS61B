060c8b9f2043c7532bc76bb61fa7dcb16c2dd293
Kern Elder <kelel1@yahoo.com>
2019-01-09 14:52:11 -0500

    complete deque interface

--- proj1b/Deque.java
+++ proj1b/Deque.java
@@ -1,3 +1,13 @@
 public interface Deque<Item> {
 
+    public void    addFirst(Item x);
+    public void    removeFirst(Item x);
+    public void    printDeque();
+    public Item    addLast(Item x);
+    public Item    removeLast();
+    public Item    getItem(int i);
+    public boolean isEmpty();
+    public int     size();
+
+
 }
